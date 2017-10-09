7128d57559dd27dd5d7bc72d1c436244a3cc4764
dlujza <daoudlujza@gmail.com>
2017-10-09 16:03:16 +0200

    Update daoud_lujza_deep_learning.py

--- daoud_lujza_deep_learning.py
+++ daoud_lujza_deep_learning.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep  7 17:24:10 2017
-
-@author: Lujza Daoud 
-"""
 import sys
 import math
 import networkx
@@ -15,8 +9,7 @@ np.set_printoptions(threshold=np.inf)
 kepek_test1 = color.rgb2gray(io.imread('eight3.png'))
 kepek_test2 = color.rgb2gray(io.imread('zero2.png'))
 kepek_expanded1 = np.expand_dims(kepek_test1,axis=0)
-kepek_expanded2 = np.expand_dims(kepek_test2,axis=0)    #now shape (1,28,28)
-print (kepek_expanded.shape)
+kepek_expanded2 = np.expand_dims(kepek_test2,axis=0) 
 x1 = np.reshape(kepek_expanded1,(1,784))
 x1=np.array(np.concatenate(x1))
 
@@ -29,10 +22,8 @@ def kepek_mashogy(kep):
     lista=[]
     for i in kep:
         if (i==1):
-            print("o")
             lista.append(index)
         index+=1
-    print(lista)
     index=0
     for i in kep:
         if index in lista:
@@ -46,17 +37,13 @@ def vane_X(): #korbejar egy X-et (eloszor \ majd a / szarat)
     sor=28
     i=(28*9)+10
     X.append(i)
-    #while (i==((28*15)+(3*28/4+1))):
     for a in range(0,10):
         i+=oszlop+sor
         X.append(i)
-        #print ("h")
     oszlop=-oszlop
     j=(28*9)+18
     X.append(j)
-    #while (j==(28*15)+(28/4+1)):
     for b in range(0,10):
-        #print ("f")
         j+=oszlop+sor
         X.append(j)
     X=[int(x) for x in X]
@@ -64,25 +51,18 @@ def vane_X(): #korbejar egy X-et (eloszor \ majd a / szarat)
 
 def ertekadas_X(X_vektor):
     filter=np.zeros(784)
-    #print (filter)
     index=0
     for i in filter:
         if index in X_vektor:
-            #print("s")
             i=i+5
-            #print (i)
             filter[index]+=5
-            #print (filter)
         index+=1
     return filter
     
 x1=kepek_mashogy(x1)
 x2=kepek_mashogy(x2)
-print (x1)
 X_vektor=vane_X()
-print (X_vektor)
 filter_vektor=ertekadas_X(X_vektor)
-#print (valami)
 filter_vektor = np.array(filter_vektor)
 
 kep_01=np.dot(x1,filter_vektor)
@@ -90,7 +70,6 @@ kep_02=np.dot(x2, filter_vektor)
 print(kep_01)
 print(kep_02)
 
-#print(valami.shape)
     
 
 
